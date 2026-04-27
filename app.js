57c6fcb6be8a4705cb082ee8f3166469d0ded034
mazen <mazenmahmod397@gmail.com>
2026-04-27 18:00:12 +0300

    fix a new bug in the code

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 const mongoose = require("mongoose");
 const Admin = require("./models/adminSchema");
 const User = require("./models/customerSchema");
